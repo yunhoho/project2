9af47eab05285aff5e1e3bd044cb1a62543e43ad
owner <owner@DESKTOP-M17INPF>
2019-09-11 16:10:12 +0900

    user package

--- project2/src/kr/co/sist/user/view/Booking.java
+++ project2/src/kr/co/sist/user/view/Booking.java
@@ -1,4 +1,4 @@
-package kr.co.sist.user;
+package kr.co.sist.user.view;
 
 import java.awt.Color;
 import java.awt.Font;
--- project2/src/kr/co/sist/user/view/UserPrice.java
+++ project2/src/kr/co/sist/user/view/UserPrice.java
@@ -1,4 +1,4 @@
-package kr.co.sist.user;
+package kr.co.sist.user.view;
 
 import java.awt.BorderLayout;
 
