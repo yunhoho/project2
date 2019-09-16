4b51d56577b51775de043fe3e93384192b15599c
USER <USER@DESKTOP-G8PIVB9>
2019-09-16 14:33:33 +0900

    패키지

---
+++ project2/src/kr/co/sist/server/controller/AdminPassEvt.java
@@ -0,0 +1,14 @@
+package kr.co.sist.server.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminPassEvt implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project2/src/kr/co/sist/server/controller/CustomerDataEvt.java
@@ -0,0 +1,15 @@
+package kr.co.sist.server.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+
+public class CustomerDataEvt extends MouseAdapter implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
